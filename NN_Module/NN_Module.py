17c743e09ca1a1800ac128f1cf29b0baf0e16de4
Polina-Stepanova <112864424+Polina-Stepanova@users.noreply.github.com>
2025-09-11 06:38:10 +0300

    Update NN_Module.py
    Добавление исключения триплетов с искаженными при извлечении моделью словами (в extract_triplets_typed) + вспомогательные функции для этого, исправление параметра py2_morph в функциях extract_triplets_typed и normalize_NP, исправление функции normalize_NP.

--- NN_Module/NN_Module.py
+++ NN_Module/NN_Module.py
@@ -70,12 +70,12 @@ for substrtext_sent in model_inputs_razd :
     mREBEL_rez_list.append(substrtext_sent.text)
 
 
-def normalize_NP(np_src,py2_morph):
+def normalize_NP(np_src,py2_morph): # исправлено имя локальной переменной в соответствии с именем параметра
   ind=0
   ind2=0
   taggen=''
   for ind  in range(len(np_src)):
-       p2=morph.parse(np_src[ind])[0]
+       p2=py2_morph.parse(np_src[ind])[0]
        if  ((p2.tag.POS=='ADJF')|(p2.tag.POS=='ADJS')|(p2.tag.POS== 'PRTF')|(p2.tag.POS== 'PRTS')):#прилагательное или причастие, полное/краткое
            np_src[ind]=p2.inflect({'sing', 'nomn'}).word
        elif  (p2.tag.POS=='NPRO'):
@@ -84,7 +84,7 @@ def normalize_NP(np_src,py2_morph):
              if (not (tempword==None)):
                 np_src[ind]=tempword.word
              else:
-                np_src[ind]=morph.normal_forms(p2.word)[0]
+                np_src[ind]=py2_morph.normal_forms(p2.word)[0]
        elif (p2.tag.POS=='NOUN'):
            taggen=p2.tag.gender
            ind2=ind
@@ -92,23 +92,24 @@ def normalize_NP(np_src,py2_morph):
            if (not (tempword==None)):
              np_src[ind]=tempword.word
            break
-       else: np_src[ind]= morph.normal_forms(p2.word)[0]
+       else: np_src[ind]= py2_morph.normal_forms(p2.word)[0]
 
   if ((taggen=='masc')|(taggen=='femn')|(taggen=='neut')):
     for ind in range(0,ind2):
-      p2=morph.parse(np_src[ind])[0]
+      p2=py2_morph.parse(np_src[ind])[0]
       if  ((p2.tag.POS=='ADJF')|(p2.tag.POS=='ADJS')|(p2.tag.POS== 'PRTF')|(p2.tag.POS== 'PRTS')):
         tempword=p2.inflect({taggen, 'sing', 'nomn'}).word
         if (not (tempword==None)):
           np_src[ind]=tempword
     tail_only_adj=0
     for ind in range(ind2+1,len(np_src)):
+      p2=py2_morph.parse(np_src[ind])[0] # исправлена отсутствовавшая строка
       if ((p2.tag.POS!='ADJF')&(p2.tag.POS!='ADJS')&(p2.tag.POS!= 'PRTF')&(p2.tag.POS!= 'PRTS')):
         tail_only_adj=1
         break
     if (tail_only_adj==0):#если это "лилия белая"
         for ind in range(ind2,len(np_src)):
-               p2=morph.parse(np_src[ind])[0]
+               p2=py2_morph.parse(np_src[ind])[0]
                if  ((p2.tag.POS=='ADJF')|(p2.tag.POS=='ADJS')|(p2.tag.POS== 'PRTF')|(p2.tag.POS== 'PRTS')):
                   tempword=p2.inflect({taggen, 'sing', 'nomn'}).word
                   if (not (tempword==None)):
@@ -128,8 +129,50 @@ roles = dict({'instance of':'экземпляр','parent taxon':'вид','subcla
 
 relations_with_reversed_order=["part of","subclass of","instance of","parent taxon","facet of"]#виды отношений, для которых нужно исправлять порядок главного и подчиненного терминов
 
+
+def separate_word_normalize(word_list_src,pm2_morph): # новая функция получения по списку слов списка их нормальных форм
+  word_list_norm=[]
+  for ind  in range(len(word_list_src)):
+    word_list_norm.append(pm2_morph.parse(word_list_src[ind])[0].normal_form)  # [0] - первый вариант морфологического разбора от морф. анализатора
+  return word_list_norm
+
+def check_for_nonexistent_words(wordsplit1, wordsplit2, substrtext_sent, py2_morph): # новая функция проверки наличия искажений в извлеченных моделью терминах (извлечение слова, которого не было в исходном тексте), возвращающая true/false
+  no_nonexistent_words_flag=True
+  substrtext_sent_split=[_.text.strip() for _ in list(tokenize(substrtext_sent.strip()))]
+
+  for ws1_w in wordsplit1:  # проверка первого термина на искажения
+    if ws1_w not in substrtext_sent_split:
+      no_nonexistent_words_flag=False
+      break
+
+  if no_nonexistent_words_flag: # если нет искажений в первом - проверка второго термина
+    for ws2_w in wordsplit2:
+      if ws2_w not in substrtext_sent_split:
+        no_nonexistent_words_flag=False
+        break
+
+  wordsplit1_byword=separate_word_normalize(wordsplit1,py2_morph) # получение списков слов в начальной форме
+  wordsplit2_byword=separate_word_normalize(wordsplit2,py2_morph)
+
+  if not no_nonexistent_words_flag: # если искажения - уточнение, не просто ли это различие формы слова при генерации моделью выходного текста
+    no_nonexistent_words_flag=True
+    substrtext_sent_split = separate_word_normalize(substrtext_sent_split,py2_morph)
+
+    for ws1_w in wordsplit1_byword:  # проверка первого термина (после пословной нормализации) на искажения
+      if ws1_w not in substrtext_sent_split:
+        no_nonexistent_words_flag=False
+        break
+
+    if no_nonexistent_words_flag: # если нет искажений в первом - проверка второго термина (после пословной нормализации)
+      for ws2_w in wordsplit2_byword:
+        if ws2_w not in substrtext_sent_split:
+          no_nonexistent_words_flag=False
+          break
+
+  return no_nonexistent_words_flag # флаг будет False только при нахождении искажений при обеих проверках.
+
 #основано на предоставленной авторами модели mREBEL функции, преобразующей строку на выходе нейросети в словарь элементов-результатов (https://huggingface.co/Babelscape/mrebel-large)
-def extract_triplets_typed(text,substrtext_sent,df_csv_receiver_table,extracted_seq_num):
+def extract_triplets_typed(text,substrtext_sent,df_csv_receiver_table,extracted_seq_num,py2_morph): # добавлен параметр, соответствующий морфологическому анализатору
     relation = ''
     text = text.strip()
     current = 'x'
@@ -139,30 +182,32 @@ def extract_triplets_typed(text,substrtext_sent,df_csv_receiver_table,extracted_
         if token == "<triplet>" or token == "<relation>":
             current = 't'
             if relation != '':
-
+                #начало дополнения
                 wordsplit1=[_.text.strip() for _ in list(tokenize(subject.strip()))]
-                wordsplit1=normalize_NP(wordsplit1,morph)
-                s1=' '.join(wordsplit1)
                 wordsplit2=[_.text.strip() for _ in list(tokenize(object_.strip()))]
-                wordsplit2=normalize_NP(wordsplit2,morph)
-                s2=' '.join(wordsplit2)
 
-                relnamesrc= relation.strip()
-                
-                #if((relnamesrc=="part of")|(relnamesrc=="subclass of")|(relnamesrc=="instance of")|(relnamesrc=="parent taxon")|(relnamesrc=="facet of")): #определенные виды отношений, требующие изменения порядка терминов
-                if (relnamesrc in relations_with_reversed_order):
-                  role2= roles.get(relnamesrc,relnamesrc)
-                  s1,s2=s2,s1
-                else: role2= roles.get(relnamesrc,relnamesrc) #если для данного вида отношения нет адаптированной на русский роли, то оно не переименовывается
+                if (check_for_nonexistent_words(wordsplit1, wordsplit2, substrtext_sent, py2_morph)): # проверка на наличие искажений в терминах при извлечении моделью. True при отсутствии искажений.
 
-                checkrepeats=df_csv_receiver_table[( df_csv_receiver_table['SubRole']==role2 )&(df_csv_receiver_table['MainTerm']==s1)&( df_csv_receiver_table['SubTerm']==s2)]
-                if not(checkrepeats.empty):
-                   df_csv_receiver_table['SrcFragment'][list(checkrepeats.index.values)[0]]=df_csv_receiver_table['SrcFragment'][list(checkrepeats.index.values)[0]]+';_; '+substrtext_sent 
-                   df_csv_receiver_table['ExtractedSeqNum'][list(checkrepeats.index.values)[0]]=df_csv_receiver_table['ExtractedSeqNum'][list(checkrepeats.index.values)[0]]+';_; '+extracted_seq_num 
-                else:
-                  if((s1.upper().isupper())&(s2.upper().isupper())&(s1!=s2)):#результаты с терминами, не содержащими букв, и с отношениям между двумя одинаковыми терминами (а - отношение - а) отбрасываются
-                    df_csv_receiver_table.loc[len(df_csv_receiver_table)]=[s1,role2,s2,substrtext_sent,extracted_seq_num]
+                  wordsplit1=normalize_NP(wordsplit1,py2_morph)
+                  s1=' '.join(wordsplit1)
+                  wordsplit2=normalize_NP(wordsplit2,py2_morph)
+                  s2=' '.join(wordsplit2)
 
+                  relnamesrc= relation.strip()
+                  #if((relnamesrc=="part of")|(relnamesrc=="subclass of")|(relnamesrc=="instance of")|(relnamesrc=="parent taxon")|(relnamesrc=="facet of")): #определенные виды отношений, требующие изменения порядка терминов
+                  if (relnamesrc in relations_with_reversed_order):
+                    role2= roles.get(relnamesrc,relnamesrc)
+                    s1,s2=s2,s1
+                  else: role2= roles.get(relnamesrc,relnamesrc) #если для данного вида отношения нет адаптированной на русский роли, то оно не переименовывается
+
+                  checkrepeats=df_csv_receiver_table[( df_csv_receiver_table['SubRole']==role2 )&(df_csv_receiver_table['MainTerm']==s1)&( df_csv_receiver_table['SubTerm']==s2)]
+                  if not(checkrepeats.empty):
+                    df_csv_receiver_table['SrcFragment'][list(checkrepeats.index.values)[0]]=df_csv_receiver_table['SrcFragment'][list(checkrepeats.index.values)[0]]+';_; '+substrtext_sent
+                    df_csv_receiver_table['ExtractedSeqNum'][list(checkrepeats.index.values)[0]]=df_csv_receiver_table['ExtractedSeqNum'][list(checkrepeats.index.values)[0]]+';_; '+extracted_seq_num
+                  else:
+                    if((s1.upper().isupper())&(s2.upper().isupper())&(s1!=s2)):#результаты с терминами, не содержащими букв, и с отношениям между двумя одинаковыми терминами (а - отношение - а) отбрасываются
+                      df_csv_receiver_table.loc[len(df_csv_receiver_table)]=[s1,role2,s2,substrtext_sent,extracted_seq_num]
+                #конец дополнения
 
                 relation = ''
             subject = ''
@@ -185,34 +230,34 @@ def extract_triplets_typed(text,substrtext_sent,df_csv_receiver_table,extracted_
             elif current == 'o':
                 relation += ' ' + token
     if subject != '' and relation != '' and object_ != '' and object_type != '' and subject_type != '':
-
+        #начало дополнения
         wordsplit1=[_.text.strip() for _ in list(tokenize(subject.strip()))]
-
-        wordsplit1=normalize_NP(wordsplit1,morph)
-        s1=' '.join(wordsplit1)
-
-
         wordsplit2=[_.text.strip() for _ in list(tokenize(object_.strip()))]
-        wordsplit2=normalize_NP(wordsplit2,morph)
-        s2=' '.join(wordsplit2)
 
-        relnamesrc= relation.strip()
-        if (relnamesrc in relations_with_reversed_order):
-          role2=roles.get(relnamesrc,relnamesrc)
-          s1,s2=s2,s1
-        else: role2= roles.get(relnamesrc,relnamesrc)
+        if (check_for_nonexistent_words(wordsplit1, wordsplit2, substrtext_sent, py2_morph)): # проверка на наличие искажений в терминах при извлечении моделью. True при отсутствии искажений.
 
-        checkrepeats=df_csv_receiver_table[( df_csv_receiver_table['SubRole']==role2 )&(df_csv_receiver_table['MainTerm']==s1)&( df_csv_receiver_table['SubTerm']==s2)]
-        if not(checkrepeats.empty):
-           df_csv_receiver_table['SrcFragment'][list(checkrepeats.index.values)[0]]=df_csv_receiver_table['SrcFragment'][list(checkrepeats.index.values)[0]]+';_; '+substrtext_sent
-           df_csv_receiver_table['ExtractedSeqNum'][list(checkrepeats.index.values)[0]]=df_csv_receiver_table['ExtractedSeqNum'][list(checkrepeats.index.values)[0]]+';_; '+extracted_seq_num
-        else:
-          if((s1.upper().isupper())&(s2.upper().isupper())&(s1!=s2)):
-            df_csv_receiver_table.loc[len(df_csv_receiver_table)]=[s1,role2,s2,substrtext_sent,extracted_seq_num] 
+          wordsplit1=normalize_NP(wordsplit1,py2_morph)
+          s1=' '.join(wordsplit1)
+          wordsplit2=normalize_NP(wordsplit2,py2_morph)
+          s2=' '.join(wordsplit2)
+
+          relnamesrc= relation.strip()
+          if (relnamesrc in relations_with_reversed_order):
+            role2=roles.get(relnamesrc,relnamesrc)
+            s1,s2=s2,s1
+          else: role2= roles.get(relnamesrc,relnamesrc)
+
+          checkrepeats=df_csv_receiver_table[( df_csv_receiver_table['SubRole']==role2 )&(df_csv_receiver_table['MainTerm']==s1)&( df_csv_receiver_table['SubTerm']==s2)]
+          if not(checkrepeats.empty):
+            df_csv_receiver_table['SrcFragment'][list(checkrepeats.index.values)[0]]=df_csv_receiver_table['SrcFragment'][list(checkrepeats.index.values)[0]]+';_; '+substrtext_sent
+            df_csv_receiver_table['ExtractedSeqNum'][list(checkrepeats.index.values)[0]]=df_csv_receiver_table['ExtractedSeqNum'][list(checkrepeats.index.values)[0]]+';_; '+extracted_seq_num
+          else:
+            if((s1.upper().isupper())&(s2.upper().isupper())&(s1!=s2)):
+              df_csv_receiver_table.loc[len(df_csv_receiver_table)]=[s1,role2,s2,substrtext_sent,extracted_seq_num]
+           #конец дополнения
 
 
 df_csv = pd.DataFrame(columns=['MainTerm','SubRole','SubTerm','SrcFragment','ExtractedSeqNum'])
-
 
 #получение из строк-результатов извлечения нейросетью отдельных пар терминов и отношений и занесение в таблицу. Для каждого предложения предполагается ТРИ извлечения. 
 #Предполагается структура списка результатов в группах по 7 вида ("номер извлечения(idx)"+"извлечение(sentence)") * 3 + "исходное предложение(orig_line)"
@@ -229,3 +274,4 @@ for i in range(0,len(mREBEL_rez_list),7):
   extract_triplets_typed(sentence,orig_line,df_csv,idx)
 
 df_csv.to_csv(name_csv,index=False)
+
